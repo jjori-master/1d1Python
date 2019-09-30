426073db79eb3d67431ef85cc87b3a7004cea6f7
jjori-master <jjori.master@gmail.com>
2019-09-30 21:29:36 +0900

    String validators
    기본 내장된 문자열 검증 메소드를 이용한 문제 풀이

--- src/hacker_rank/challenges/strings.py
+++ src/hacker_rank/challenges/strings.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 def swap_case(str):
     swap_char_list = []
     for c in str:
@@ -5,3 +6,76 @@ def swap_case(str):
         swap_char_list.append(swap_char)
 
     return ''.join(swap_char_list)
+
+
+'''https://www.hackerrank.com/challenges/string-validators/problem?h_r=next-challenge&h_v=zen
+
+Python has built-in string validation methods for basic data.
+파이썬에는 기본 데이터에 대한 내장 문자열 유효성 검사 방법이 있습니다
+
+It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+문자열이 알파벳 문자, 영숫자 문자, 숫자 등으로 구성되어 있는지 확인할 수 있습니다.
+
+Task:
+You are given a string S.
+Your task is to find out if the string S contains:
+alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+문자열에 영숫자, 영문자, 숫자, 소문자 및 대문자가 포함되어 있는지 확인해야합니다.
+
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+첫번째 줄은 S에 영숫문자가 존재할때 True 아닐때 False
+
+In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+두번째 줄은 S에 문자가 있을 경우 True 아닐경우 False
+
+In the third line, print True if  has any digits. Otherwise, print False.
+세번째 줄은 S에 숫자가 있을 경우 True 아닐 경우 False
+
+In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+네번째 줄은 소문자가 있을 경우 True 아닐 경우 False
+
+In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+다섯째 줄은 대문자가 있을 경우 True 아닐 경우 False'''
+
+
+def is_contains_alnum(S):
+    for s in S:
+        if s.isalnum():
+            return True
+    return False
+
+
+def is_contains_alpha(S):
+    for s in S:
+        if s.isalpha():
+            return True
+    return False
+
+
+def is_contains_digit(S):
+    for s in S:
+        if s.isdigit():
+            return True
+    return False
+
+
+def is_contains_lower(S):
+    for s in S:
+        if s.islower():
+            return True
+    return False
+
+
+def is_contains_upper(S):
+    for s in S:
+        if s.isupper():
+            return True
+    return False
+
+
+def string_validators(S):
+    print(is_contains_alnum(S))
+    print(is_contains_alpha(S))
+    print(is_contains_digit(S))
+    print(is_contains_lower(S))
+    print(is_contains_upper(S))
--- tests/challenges/strings_test.py
+++ tests/challenges/strings_test.py
@@ -8,5 +8,30 @@ def test_swap_case():
     res = swap_case('Pythonist 2')
     assert res == 'pYTHONIST 2'
 
-    res = swap_case("OzD0ro.7xwq A.MqhqT7'evgaVjpQ36OjSwfhuMaME'SDlyjPttjr6knZURciDARzAZ.3hQeIvX'IPe875zS3su6dajlMnbQOiH37U3YG0uzy6vW4v.onodBDDREYOf3CqeHfGcCeWV0dXrmTWAUsuw'jVwFO'5n.jvKLwXaDsam'y5sVQqB5O16bn1ug1XFeg3kmOGQFjxQ524k Z0yv76b4kkaC YuW9cw8Sfjf4 ynhe N88N3N2pfJF.I4ec5PgTp0Mp6VKawzg7GctsbPmU73aERqHxvWOMxDGZI9XWnJV6Aumecv0bHopAmfeD1K vTcxkzmdypHOGcdELdXjINde.ICPhEnpr9nJVL M7uY'L0BDX0i7CNCjIt9OiE2xjXt7JkK7vdUFlp6qn3Zua5vfP'nJcbviLrwj3.VlDDkd4VQ6zV1G2sGMmTVYkzaR9z5CDujAl'zYw8jw 9VOb5dP'zT'UTLMX2u8OG5YrGrvcENXF'QhBC8Ti'KRuhuO'SLlDKuRPHxFtnXW27YY1BMN9YkQww9kloEb43'loYiSFpIPiA0f1.3RYNUEN4QSbNQMpwQPZXsVKhc.uGvGJHiU80gcvOufOI2A0LdPcwGBWjTDl4TBZaycMfCmOceYqPj gqV7wTfSkY1F4INWqlKez.Vzbf84htDJg6D4MUt 2cGy8gMQolE 3gLR.rh3s5nbzN8hqKJr4c0L48zm2Hm7ObgsDu8dlrHnDaVKLQ65a7oTyz D6MLYD.q2AQXnQHKuTxKu5Fq7iNIRZKYyYeZo8n8JwHXtDbOaEnu'4'vmAsf2XR2q0ozLdw2FVsVpKEm4KzigoxY2GOpZq3OYW8cboQoD7STPJ8B'yEGZgk0vT mBABj0xeckqn6'Ouo99aTq'LKILUYjJ3DrduZj6Gq25kHUhfskez4LjZ21jduxXVXa8EmBNQeMf1A4R1v3fqGl W4Aw2UZ0t62TM fDGTqWgfvTb1RP7wyJ")
+    res = swap_case(
+        "OzD0ro.7xwq A.MqhqT7'evgaVjpQ36OjSwfhuMaME'SDlyjPttjr6knZURciDARzAZ.3hQeIvX'IPe875zS3su6dajlMnbQOiH37U3YG0uzy6vW4v.onodBDDREYOf3CqeHfGcCeWV0dXrmTWAUsuw'jVwFO'5n.jvKLwXaDsam'y5sVQqB5O16bn1ug1XFeg3kmOGQFjxQ524k Z0yv76b4kkaC YuW9cw8Sfjf4 ynhe N88N3N2pfJF.I4ec5PgTp0Mp6VKawzg7GctsbPmU73aERqHxvWOMxDGZI9XWnJV6Aumecv0bHopAmfeD1K vTcxkzmdypHOGcdELdXjINde.ICPhEnpr9nJVL M7uY'L0BDX0i7CNCjIt9OiE2xjXt7JkK7vdUFlp6qn3Zua5vfP'nJcbviLrwj3.VlDDkd4VQ6zV1G2sGMmTVYkzaR9z5CDujAl'zYw8jw 9VOb5dP'zT'UTLMX2u8OG5YrGrvcENXF'QhBC8Ti'KRuhuO'SLlDKuRPHxFtnXW27YY1BMN9YkQww9kloEb43'loYiSFpIPiA0f1.3RYNUEN4QSbNQMpwQPZXsVKhc.uGvGJHiU80gcvOufOI2A0LdPcwGBWjTDl4TBZaycMfCmOceYqPj gqV7wTfSkY1F4INWqlKez.Vzbf84htDJg6D4MUt 2cGy8gMQolE 3gLR.rh3s5nbzN8hqKJr4c0L48zm2Hm7ObgsDu8dlrHnDaVKLQ65a7oTyz D6MLYD.q2AQXnQHKuTxKu5Fq7iNIRZKYyYeZo8n8JwHXtDbOaEnu'4'vmAsf2XR2q0ozLdw2FVsVpKEm4KzigoxY2GOpZq3OYW8cboQoD7STPJ8B'yEGZgk0vT mBABj0xeckqn6'Ouo99aTq'LKILUYjJ3DrduZj6Gq25kHUhfskez4LjZ21jduxXVXa8EmBNQeMf1A4R1v3fqGl W4Aw2UZ0t62TM fDGTqWgfvTb1RP7wyJ")
     assert res == "oZd0RO.7XWQ a.mQHQt7'EVGAvJPq36oJsWFHUmAme'sdLYJpTTJR6KNzurCIdarZaz.3HqEiVx'ipE875Zs3SU6DAJLmNBqoIh37u3yg0UZY6Vw4V.ONODbddreyoF3cQEhFgCcEwv0DxRMtwauSUW'JvWfo'5N.JVklWxAdSAM'Y5SvqQb5o16BN1UG1xfEG3KMogqfJXq524K z0YV76B4KKAc yUw9CW8sFJF4 YNHE n88n3n2PFjf.i4EC5pGtP0mP6vkAWZG7gCTSBpMu73AerQhXVwomXdgzi9xwNjv6aUMECV0BhOPaMFEd1k VtCXKZMDYPhogCDelDxJinDE.icpHeNPR9Njvl m7Uy'l0bdx0I7cncJiT9oIe2XJxT7jKk7VDufLP6QN3zUA5VFp'NjCBVIlRWJ3.vLddKD4vq6Zv1g2SgmMtvyKZAr9Z5cdUJaL'ZyW8JW 9voB5Dp'Zt'utlmx2U8og5yRgRVCenxf'qHbc8tI'krUHUo'slLdkUrphXfTNxw27yy1bmn9yKqWW9KLOeB43'LOyIsfPipIa0F1.3rynuen4qsBnqmPWqpzxSvkHC.UgVgjhIu80GCVoUFoi2a0lDpCWgbwJtdL4tbzAYCmFcMoCEyQpJ GQv7WtFsKy1f4inwQLkEZ.vZBF84HTdjG6d4muT 2CgY8GmqOLe 3Glr.RH3S5NBZn8HQkjR4C0l48ZM2hM7oBGSdU8DLRhNdAvklq65A7OtYZ d6mlyd.Q2aqxNqhkUtXkU5fQ7InirzkyYyEzO8N8jWhxTdBoAeNU'4'VMaSF2xr2Q0OZlDW2fvSvPkeM4kZIGOXy2goPzQ3oyw8CBOqOd7stpj8b'YegzGK0Vt MbabJ0XECKQN6'oUO99AtQ'lkiluyJj3dRDUzJ6gQ25KhuHFSKEZ4lJz21JDUXxvxA8eMbnqEmF1a4r1V3FQgL w4aW2uz0T62tm FdgtQwGFVtB1rp7WYj"
+
+
+def test_string_validators(capsys):
+    string_validators('qA2')
+
+    captured = capsys.readouterr()
+    spliced_captured_out = captured.out.split("\n")[:5]
+
+    assert spliced_captured_out[0] == 'True'
+    assert spliced_captured_out[1] == 'True'
+    assert spliced_captured_out[2] == 'True'
+    assert spliced_captured_out[3] == 'True'
+    assert spliced_captured_out[4] == 'True'
+
+    string_validators('#$%@^&*')
+
+    captured = capsys.readouterr()
+    spliced_captured_out = captured.out.split("\n")[:5]
+
+    assert spliced_captured_out[0] == 'False'
+    assert spliced_captured_out[1] == 'False'
+    assert spliced_captured_out[2] == 'False'
+    assert spliced_captured_out[3] == 'False'
+    assert spliced_captured_out[4] == 'False'
