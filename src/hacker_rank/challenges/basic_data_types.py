fc0630ce579400b413a136f73a05cef779cd2192
jjori-master <jjori.master@gmail.com>
2019-10-12 23:38:04 +0900

    nested lists

--- src/hacker_rank/challenges/basic_data_types.py
+++ src/hacker_rank/challenges/basic_data_types.py
@@ -22,3 +22,20 @@ def lists(list, command):
         return
 
     method_to_call()
+
+
+# 두번째로 낮은 점수를 가지고 있는 학생 이름을 출력
+def nested_lists(li):
+    scores = list(set(map(lambda i: i[1], li)))
+    scores.sort()
+    second_score = scores[1]
+
+    def match_second_score(info):
+        return second_score == info[1]
+
+    second_score_list = list(filter(match_second_score, li))
+    second_score_names = list(set(map(lambda i: i[0], second_score_list)))
+    second_score_names.sort()
+
+    for name in second_score_names:
+        print(name)
--- tests/challenges/basic_data_types_test.py
+++ tests/challenges/basic_data_types_test.py
@@ -15,3 +15,23 @@ def test_lists(capsys):
     assert spliced_captured_out[0] == '[6, 5, 10]'
     assert spliced_captured_out[1] == '[1, 5, 9, 10]'
     assert spliced_captured_out[2] == '[9, 5, 1]'
+
+
+def test_nested_lists(capsys):
+    li = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harash', 39]]
+    nested_lists(li)
+
+    captured = capsys.readouterr()
+    spliced_captured_out = captured.out.split("\n")[:2]
+
+    assert spliced_captured_out[0] == 'Berry'
+    assert spliced_captured_out[1] == 'Harry'
+
+    li = [['Harsh', 20], ['Beria', 20], ['Varun', 19], ['Kakunami', 19], ['Vikas', 21]]
+    nested_lists(li)
+
+    captured = capsys.readouterr()
+    spliced_captured_out = captured.out.split("\n")[:2]
+
+    assert spliced_captured_out[0] == 'Beria'
+    assert spliced_captured_out[1] == 'Harsh'
