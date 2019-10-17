988241932f569df957e6ff723572911f913837e5
jjori <jjori.master@gmail.com>
2019-10-17 22:03:02 +0900

    collatz 추측 풀기 난이도 쉬움

--- tests/programmers/problem_solving_learning_test.py
+++ tests/programmers/problem_solving_learning_test.py
@@ -7,3 +7,14 @@ def test_hide_phone_number():
 
     number = hide_phone_number('0103334443')
     assert number == '******4443'
+
+
+def test_collatz_solution():
+    answer = collatz_solution(6)
+    assert answer == 8
+
+    answer = collatz_solution(16)
+    assert answer == 4
+
+    answer = collatz_solution(626331)
+    assert answer == -1
